7f2c47d9bbdd400050412148d3adfd31b7802854
Mokshy46 <mokshithgowda88@gmail.com>
2026-09-23 09:49:16 +0000

    fixed image upload bug

--- backend/reports/serializers.py
+++ backend/reports/serializers.py
@@ -4,9 +4,28 @@ from .models import DogReport
 
 class DogReportModelSerializer(serializers.ModelSerializer):
     
+    reported_by_username = serializers.CharField( source = "reported_by.first_name", read_only = True)
+    dog_tag = serializers.CharField(source = "dog.tag_id", read_only = True)
+    
     class Meta:
         model = DogReport
-        fields = '__all__'
+        fields = [
+            "original_image",
+            "description",
+            "latitude",
+            "longitude",
+            "location_name",
+            "status",
+            "created_at",
+            "updated_at",
+            "resolved_at",
+            "dog",
+            "dog_tag",
+            "reported_by",
+            "reported_by_username",
+            "assigned_to",
+            
+        ]
         
 class DogReportCreateSerializer(serializers.ModelSerializer):
     
